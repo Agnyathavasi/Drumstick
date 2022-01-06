7e3f758f0c8f079ccdf3cf80ceb7221440b4893b
Vishwajith H H <65083607+Agnyathavasi@users.noreply.github.com>
2022-01-06 13:30:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+function clickw () {
+  var audio = new Audio('sounds/crash.mp3');
+  audio.play();
+}
+
+function clicka () {
+  var audio = new Audio('sounds/kick-bass.mp3');
+  audio.play();
+}
+
+function clicks () {
+  var audio = new Audio('sounds/snare.mp3');
+  audio.play();
+}
+
+function clickd () {
+  var audio = new Audio('sounds/tom-1.mp3');
+  audio.play();
+}
+
+function clickj () {
+  var audio = new Audio('sounds/tom-2.mp3');
+  audio.play();
+}
+
+function clickk () {
+  var audio = new Audio('sounds/tom-3.mp3');
+  audio.play();
+}
+
+function clickl () {
+  var audio = new Audio('sounds/tom-4.mp3');
+  audio.play();
+}
